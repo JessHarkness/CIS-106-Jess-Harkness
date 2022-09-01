8f9e1ca82ef33f25ee49420b23a18f4bc5d5c684
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-01 11:15:09 -0400

    Input & Variable name change
    The original upload included my test input value. New update: changed test input value to "input()". Changed "m" to "meters" to satisfy requirement for meaningful variable names.

--- Assignment 3/Activity 3.py
+++ Assignment 3/Activity 3.py
@@ -1,8 +1,8 @@
 # This program converts an input distance in miles to distance in kilometers, meters, and centimeters.
-miles = float(26.2)
+miles = float(input())
 km = miles * 1.609344
-m = km * 1000
-cm = m * 100
+meters = km * 1000
+cm = meters * 100
 print("Kilometers:" + str(km))
-print("Meters:" + str(m))
+print("Meters:" + str(meters))
 print("Centimeters:" + str(cm))
