2e929d96f62894670b3afe2c315676742682970b
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-01 11:30:44 -0400

    Source Code Update 3
    Update includes: user prompt message, variable name change for "meters"

--- Assignment 3/Activity 3.py
+++ Assignment 3/Activity 3.py
@@ -1,8 +1,9 @@
 # This program converts an input distance in miles to distance in kilometers, meters, and centimeters.
+print("Please enter a distance in miles.")
 miles = float(input())
 km = miles * 1.609344
-meters = km * 1000
-cm = meters * 100
+metrs = km * 1000
+cm = metrs * 100
 print("Kilometers:" + str(km))
-print("Meters:" + str(meters))
+print("Meters:" + str(metrs))
 print("Centimeters:" + str(cm))
