2e6e18fd3c827bf90318fecf3a995b121eb592ab
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-01 11:33:19 -0400

    Source Code References
    Update includes: reference comment within the source code.

--- Assignment 3/Activity 3.py
+++ Assignment 3/Activity 3.py
@@ -1,3 +1,4 @@
+# References: https://www.youtube.com/watch?v=FWaN3lTyhPU I used this video to help me create functions in Flowgorithm.
 # This program converts an input distance in miles to distance in kilometers, meters, and centimeters.
 print("Please enter a distance in miles.")
 miles = float(input())
