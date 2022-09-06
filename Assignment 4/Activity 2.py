a2a1a8c8a4498f71bd1a04a33e93c05cec2a33d1
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-06 18:19:52 -0400

    Chapter 2 Activity 2 program
    The initial upload of my Activity 2 program.

---
+++ Assignment 4/Activity 2.py
@@ -0,0 +1,8 @@
+# This program will ask a user to input their age in years and convert it to months, days, hours, and seconds.
+print("Enter your age in years.")
+age=int(input())
+months=age*12
+days=age*365
+hours=days*24
+seconds=hours*3600
+print("You are:",months, "months", ",",days, "days", ",",hours,"hours", ",",seconds,"seconds","old")
