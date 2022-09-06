7d960fa343b538be5fc90c20e63394852fdcced1
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-06 18:30:51 -0400

    Activity 2 Update 2
    Update includes: shorter character count for comments, separating output lines to meet shorter character count requirements

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -1,4 +1,4 @@
-# This program will take a user's age in years and convert it to months, days, hours, and seconds.
+# This program will convert a user's age (years) to months, days, hours, seconds.
 
 print("Enter your age in years.")
 age = int(input())
@@ -8,4 +8,5 @@ days = age * 365
 hours = days * 24
 seconds = hours * 3600
 
-print("You are:", months, "months", "," ,days, "days", "," ,hours, "hours", "," ,seconds, "seconds", "old")
+print("You are:")
+print(months, "months", days, "days", hours, "hours", seconds, "seconds", "old")
