9f283d99dd645808951ba521859fec4b52352a22
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-14 11:16:01 -0400

    Initial upload of Activity 2 source code

---
+++ Assignment 5/Activity 2.py
@@ -0,0 +1,32 @@
+def calculateKilometers(miles):
+    kilometers = miles * 1.609344
+    
+    return kilometers
+
+def calculateMeters(kilometers):
+    meters = kilometers * 1000
+    
+    return meters
+
+def displayResult(miles, kilometers, meters, centimeters):
+    print(str(miles) + " miles is: " + str(kilometers) + " kilometers, " + str(meters) + " meters, " + str(centimeters) + " centimeters. ", end='', flush=True)
+
+def calculateCentimeters(meters):
+    centimeters = meters * 100
+    
+    return centimeters
+
+def getMiles():
+    print("Enter your distance in miles:")
+    miles = float(input())
+    
+    return miles
+
+# Main
+# This program takes in input distance in miles and converts it to kilometers, meters, and centimeters.
+# References: https://www.mathisfun.com/measure/us-standard-length.html
+miles = getMiles()
+kilometers = calculateKilometers(miles)
+meters = calculateMeters(kilometers)
+centimeters = calculateCentimeters(meters)
+displayResult(miles, kilometers, meters, centimeters)
