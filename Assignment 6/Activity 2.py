2b92dc0bb2046c1605109e2ac23412f5d9a2a614
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-09-22 10:07:41 -0400

    Changes

--- Assignment 6/Activity 2.py
+++ Assignment 6/Activity 2.py
@@ -11,22 +11,26 @@ def get_years():
     
     
 def calculate_months(years):
-    months = years * 12
+    months_per_year = 12
+    months = years * months_per_year
     return (months)
 
 
 def calculate_days(years):
-    days = years * 365
+    days_per_year = 365
+    days = years * days_per_year
     return (days)
 
 
 def calculate_hours(days):
-    hours = days * 24
+    hours_per_day = 24
+    hours = days * hours_per_day
     return (hours)
 
 
 def calculate_seconds(hours):
-    seconds = hours * 60 * 60
+    seconds_per_hour = 60 * 60
+    seconds = hours * seconds_per_hour
     return (seconds)
 
 
@@ -35,17 +39,16 @@ def display_result(years, months, days,
     print(str(years) + " years is: " + str(months) + 
           " months, " + str(days) + 
           " days, " + str(hours) + " hours, " + 
-          str(seconds)+ " seconds.")
+          str(seconds) + " seconds.")
 
     
-def main_function (years, months, days, hours, seconds):
+def main_function():
     years = get_years()
     months = calculate_months(years)
     days = calculate_days(years)
     hours = calculate_hours(days)
     seconds = calculate_seconds(hours)
     display_result(years, months, days, hours, seconds)
-    return (none)
 
 
-main ()
+main_function ()
