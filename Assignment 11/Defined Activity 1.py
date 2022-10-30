b17e7b8fa64369fa04527a4b640f21cc8cc3babb
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-10-30 09:49:56 -0400

    Fixes- list placements, display function placement

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -6,7 +6,7 @@
 # References: https://realpython.com/python-modulo-operator/
 
 def main():
-    month_names_list = month_names_list = ['0', 'January', 
+    month_names_list = ['0', 'January', 
                    'February', 'March', 'April',
                    'May', 'June', 'July', 'August', 'Spetember',
                    'October', 'November', 'December']
@@ -16,9 +16,9 @@ def main():
         month_number = get_month_number()
         month_name = get_month_name(month_number, month_names_list)
         month_days = get_month_days(year, month_number, month_days_list)
-        display_days(month_name, month_days, year)
         if not (year >= 1582) or (month_number in (1, 12)):
             break
+        display_days(month_name, month_days, year)
     
     
 def get_year():
@@ -36,16 +36,12 @@ def get_month_number():
 
 
 def get_month_name(month_number, month_names_list):
-    month_names_list = ['0', 'January', 'February', 'March', 'April',
-                   'May', 'June', 'July', 'August', 'Spetember',
-                   'October', 'November', 'December']
     month_name = month_names_list[month_number]
     
     return month_name
 
 
 def get_month_days(year, month_number, month_days_list):
-    month_days_list = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     month_days = month_days_list[month_number]
     if month_number == 2 and year % 4 == 0 and year % 400 == 0:
         month_days = 29
