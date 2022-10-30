798f99d3e5d33aa2da6287261510f1670ba9c737
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-10-30 11:10:13 -0400

    Fixing condition statement

--- Assignment 11/Defined Activity 1.py
+++ Assignment 11/Defined Activity 1.py
@@ -43,14 +43,15 @@ def get_month_name(month_number, month_names_list):
 
 def get_month_days(year, month_number, month_days_list):
     month_days = month_days_list[month_number]
-    if month_number == 2 and year % 4 == 0 or year % 400 == 0 and year % 100 != 0:
+    if month_number == 2 and year % 4 == 0 and year % 100 != 0:
         month_days = 29
     
     return month_days
 
 
 def display_days(month_name, month_days, year):
-    print(str(month_name) + ' ' + str(year) + ' has ' + str(month_days) + ' days.')
+    print(str(month_name) + ' ' + str(year) 
+          + ' has ' + str(month_days) + ' days.')
     
     
 main()
