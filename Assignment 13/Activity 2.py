a22525bab031994cf69500358006dffb408da11a
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-11-11 15:24:39 -0500

    Complete upload
    First upload did not include my processing function for some reason

--- Assignment 13/Activity 2.py
+++ Assignment 13/Activity 2.py
@@ -1,18 +1,31 @@
 # This program takes a line of text and
 # displays it backwards.
+# References:
+# https://thispointer.com/remove-duplicate-spaces-from-string-in-python/
+# https://www.digitalocean.com/community/tutorials/python-remove-spaces-from-string
+
 
 def get_text():
     print("Enter a line of text.")
-    text_string = str(input())
-    
-    return text_string
+    user_text = str(input())
+    return user_text
 
-def display_string_backwards(text_string):
-    print(text_string[::-1])
+
+def process_string(user_text):
+    user_text = user_text.strip()
+    user_text = " ".join(user_text.split())
+    backwards_text = user_text[::-1]
+    return backwards_text
+
+
+def display_string(backwards_text):
+    print(str(backwards_text))
+    
     
 def main():
-    text_string = get_text()
-    display_string_backwards(text_string)
-    
+    user_text = get_text()
+    backwards_text = process_string(user_text)
+    display_string(backwards_text)
+   
+   
 main()
-    
