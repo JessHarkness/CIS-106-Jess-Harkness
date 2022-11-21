c506edde934f76734a7f2c6bfafd06d4cddc06fd
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-11-21 08:54:13 -0500

    renaming file path

--- Assignment 14/Activity 1.py
+++ Assignment 14/Activity 1.py
@@ -8,9 +8,9 @@
 # https://www.w3schools.com/python/ref_func_round.asp
 
  
-def process_scores_list('scores.txt'):
+def process_scores_list('Assignment 14/scores.txt'):
     scores_list = []
-    with open('scores.txt', 'r') as file_contents:
+    with open('Assignment 14/scores.txt', 'r') as file_contents:
         for line in file_contents:
             try:
                 score = line.strip().split(",")[1]
@@ -47,7 +47,7 @@ def display_stats(maximum, minimum, average):
    
    
 def main():
-    scores_list = process_scores_list('scores.txt')
+    scores_list = process_scores_list('Assignment 14/scores.txt')
     display_list(scores_list)
     maximum = calculate_max(scores_list)
     minimum = calculate_min(scores_list)
