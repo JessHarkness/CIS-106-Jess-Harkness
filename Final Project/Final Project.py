b16b43049c20a4c3a7a1a2d9849d01912ee47091
JessHarkness <111314105+JessHarkness@users.noreply.github.com>
2022-12-07 19:06:42 -0500

    code as of: 12/7
    processing function now accepts all tags

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -10,46 +10,48 @@ import os
 def main():
     plant_catalog = '/Users/jessharkness/Desktop/CIS 106/plant_catalog.xml'
     if os.path.isfile(plant_catalog):
+        tag = 'COMMON' or 'BOTANICAL' or 'ZONE' or 'LIGHT' or 'PRICE'
         common_names = process_lists(plant_catalog, 'COMMON')
         botanical_names = process_lists(plant_catalog, 'BOTANICAL')
         growing_zones = process_lists(plant_catalog, 'ZONE')
         light_levels = process_lists(plant_catalog, 'LIGHT')
-        plant_prices = process_lists(plant_catalog, 'PRICE')
+        plant_price = process_lists(plant_catalog, 'PRICE')
         display_catalog(common_names, botanical_names,
-                      growing_zones, light_levels, plant_prices)
-        average_price = calculate_average(plant_prices)
-        display_statistics(average_price, plant_prices)
+                      growing_zones, light_levels, plant_price)
+        average_price = calculate_average(plant_price)
+        display_statistics(average_price, plant_price)
     else:
         print("File does not exist.")
         
-def process_lists(plant_catalog):
-    common_names = []
-    botanical_names = []
-    growing_zones = []
-    light_levels = []
-    plant_prices = []
-    with open(plant_catalog, 'r') as file_contents:
-    for line in file_contents:
-        line = line.strip()
-        if any([line.startswith('<COMMON>')]):
-            common_name = line.removeprefix('<COMMON>').removesuffix('</COMMON>')
-            common_names.append(common_name)
-# repeat this function for each variable (botanical, light, zone, price)
-# for prices list, list data type will be changed to float
-# only one function is needed for processing lists, and this function
-# will be called five times (once for each list)
         
-def display_items(common_names, botanical_names, growing_zones,
-                  light_levels, plant_prices):
-    for index in range(len(common_names)):
-    print(str(common_names[index]) + ' (' +str(botanical_names[index]) + ') ' + ' - ' + str(growing_zones[index]) + ' - ' + str(light_levels[index]) + ' - $' + str(plant_prices[index]))
+def process_lists(plant_catalog, tag):
+    array = []
+    with open(plant_catalog, 'r') as file_contents:
+        for line in file_contents:
+            line = line.strip()
+            line = line.replace('<','').replace('>','').replace('/','')
+            if any([line.startswith(tag)]):
+                line = line.removeprefix(tag).removesuffix(tag)
+                array.append(line)
+    return array
 
-def calculate_average(plant_prices):
-    total = sum(plant_prices)
-    average_price = round(total / (len(plant_prices)), 2)
+
+def display_catalog(common_names, botanical_names, growing_zones,
+                  light_levels, plant_price):
+    for index in range(len(common_names)):
+        print(str(common_names[index]) + ' (' +str(botanical_names[index]) + ') ' +
+              ' - ' + str(growing_zones[index]) + ' - ' +
+              str(light_levels[index]) + ' - $' + str(plant_price[index]))
+
+
+def calculate_average(plant_price):
+    total = sum(plant_price)
+    average_price = round(total / (len(plant_price)), 2)
     return average_price
 
-def display_statistics(average_price, plant_prices):
-    print(str(len(plant_prices)) + ' items. - $' + str(average_price) + ' average price.')
+
+def display_statistics(average_price, plant_price):
+    print(str(len(plant_price)) + ' items. - $' +
+          str(average_price) + ' average price.')
  
           
